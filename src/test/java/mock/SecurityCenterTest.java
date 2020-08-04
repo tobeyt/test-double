8d2c4a2e7cac4fed9cda5167048ab2172c1b4edd
Tobey <bitsqiu@gmail.com>
2020-08-04 12:37:04 +0800

    [qd] fixed: use mockito

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -3,39 +3,32 @@ package mock;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
 
+
+@ExtendWith(MockitoExtension.class)
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
-    private static class MockDoorPanel extends DoorPanel {
-        private Boolean hasCalled = false;
-
-        @Override
-        void close() {
-            hasCalled = true;
-        }
-
-        public Boolean getHasCalled() {
-            return hasCalled;
-        }
-    }
-
+    @Mock
+    private DoorPanel mockDoorPanel;
+    @InjectMocks
     private SecurityCenter securityCenter;
-    private MockDoorPanel mockDoorPanel;
 
     @BeforeEach
     public void setUp() {
-        mockDoorPanel = new MockDoorPanel();
-        securityCenter = new SecurityCenter(mockDoorPanel);
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
         securityCenter.switchOn();
 
-        assertTrue(mockDoorPanel.getHasCalled());
+        verify(mockDoorPanel).close();
     }
 }
