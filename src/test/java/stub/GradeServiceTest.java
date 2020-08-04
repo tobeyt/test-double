2c68f9aabf063d7bf506c731642b1293fe7f1c16
Tobey <bitsqiu@gmail.com>
2020-08-04 12:32:49 +0800

    [qd] fixed: mock first.

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -7,9 +7,10 @@ public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
-
+    private GradeService gradeService;
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
+//        gradeService.calculateAverageGrades();
         //Assertions.assertEquals(90.0, result);
     }
 }
